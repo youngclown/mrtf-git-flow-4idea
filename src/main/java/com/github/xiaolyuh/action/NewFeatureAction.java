12e2bde6422604a413c7e0594125853aba344a6c
bymin <ymkim@itsdcode.com>
2021-05-26 22:42:47 +0900

    한글명으로 변경

--- src/main/java/com/github/xiaolyuh/action/NewFeatureAction.java
+++ src/main/java/com/github/xiaolyuh/action/NewFeatureAction.java
@@ -21,7 +21,8 @@ import java.util.Objects;
 public class NewFeatureAction extends AbstractNewBranchAction {
 
     public NewFeatureAction() {
-        super("新建开发分支", "新建开发分支，并推送到远程仓库", IconLoader.getIcon("/icons/feature.svg", Objects.requireNonNull(ReflectionUtil.getGrandCallerClass())));
+        super("새로운 개발 지점", "새 개발 브랜치를 생성하고 원격웨어 하우스로 푸시",
+                IconLoader.getIcon("/icons/feature.svg", Objects.requireNonNull(ReflectionUtil.getGrandCallerClass())));
     }
 
     @Override
